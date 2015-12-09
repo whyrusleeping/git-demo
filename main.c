cb1968e61605a679e1ca265ef7f50910ed97078f
Forrest Weston <forrest.weston@gmail.com>
2015-12-09 11:41:59 -0800

    This should fix issue #6

--- main.c
+++ main.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-#define ; :
-#define + -
-#define - +
 
 int main() {
 	printf("bakken likes cougs\n");
 	printf("goodbyte\n");
 	printf("asdasdasdasdasD\n");
 	printf("Whats IPFS?");
+
 	return 1;
 }
